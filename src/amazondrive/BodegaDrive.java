8e4e57b4beddcc0849d8685876e0f0e74f81e523
Horacio <Horacio@aguacristal>
2018-09-22 19:33:56 -0500

    inicial2

--- src/amazondrive/BodegaDrive.java
+++ src/amazondrive/BodegaDrive.java
@@ -10,12 +10,18 @@ import becker.robots.*;
  * @author Horacio
  */
 public class BodegaDrive {
-    private static City city=new City();
-
+    private City city=new City();
+    private  Estantes [] estante=new Estantes [20];
+    public RobotDrive [] robotdrive=new RobotDrive[10];
+   
+    
     public BodegaDrive() {
-         for (int i = 0; i < 12; i++) {
-            Wall pared = new Wall(city, 0, i+4, Direction.NORTH);
-            Wall pared1 = new Wall(city,16, i+4, Direction.SOUTH);
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        
+   //colocacion de paredes     
+        
+        for (int i = 0; i < 12; i++) {
+            Wall pared = new Wall(city, 2, i+4, Direction.NORTH);
+            Wall pared1 = new Wall(city,15, i+4, Direction.SOUTH);
     }
         for (int i = 0; i < 2; i++) {
             Wall pared = new Wall(city, i+4, 2, Direction.WEST);
@@ -28,30 +34,88 @@ public class BodegaDrive {
              Wall pared8 = new Wall(city,3 , 2+i, Direction.SOUTH);
             Wall pared9 = new Wall(city,3, 16+i, Direction.SOUTH);
               
-            Wall pared4 = new Wall(city, i+15, 4, Direction.WEST);
-            Wall pared5 = new Wall(city,i+15, 15, Direction.EAST);
-            
-        }
+            Wall pared4 = new Wall(city, 15, 4, Direction.WEST);
+            Wall pared5 = new Wall(city,15, 15, Direction.EAST);
+            }
      
-          for (int i = 0; i < 4; i++) {
-              Wall pared2 = new Wall(city, i, 4, Direction.WEST);
-            Wall pared1 = new Wall(city,i, 15, Direction.EAST);
+          for (int i = 0; i < 2; i++) {
+              Wall pared2 = new Wall(city, i+2, 4, Direction.WEST);
+            Wall pared1 = new Wall(city,i+2, 15, Direction.EAST);
        }
          for (int i = 0; i < 7; i++) {
              Wall pared6 = new Wall(city,6+i ,2 , Direction.WEST);
             Wall pared7 = new Wall(city,6+i, 17, Direction.EAST);
         }
-       
-     
             Wall pared6 = new Wall(city,14 , 1, Direction.NORTH);
             Wall pared7 = new Wall(city,14, 18, Direction.NORTH);
             Wall pared8 = new Wall(city,12 , 1, Direction.SOUTH);
             Wall pared9 = new Wall(city,12, 18, Direction.SOUTH);
-        
-          
-          
-        }
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////      
+  //colocacion de things para estantes       
+  for (int i = 2; i < 4; i++) {
+              for (int j = 5; j < 15; j++) {
+     Thing thing=new Thing(city,i,j);
+      }}
+  // int j=5;int k=0;int kj=9;         
+   //while(j<15&&k<10&&kj<20){
+   //this.estante[k].setPosicion(2, j);
+    //this.estante[kj].setPosicion(3, j);
+    //j++;k++;kj++;
+  // }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////     
+  // colocacion de los robots
+  
+   int jl=5;int kl=0;        
+   while(jl<15&&kl<10){
+   this.robotdrive[kl]=new RobotDrive(city,15,jl);
+   
+    jl++;kl++;
+   }
+  
+    
+    
+    }
+
+    public City getCity() {
+        return this.city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+    
+    public  Posicion estantesDisponibles (){
+     for (int i = 0; i < 20; i++) {
+         for (int j = 0; j < 3; j++) {
+             for (int k = 0; k < 7; k++) {
+               if (this.estante[i].getProducto()[j][k]==null&&this.estante[i].isEstado()==false){
+                 return this.estante[i].getPosicion();
+               }}}}    
+   return null; 
+}
+
+    public Estantes[] getEstante() {
+        return estante;
+    }
+
+    public void setEstante(Estantes[] estante) {
+        this.estante = estante;
+    }
+
+    public RobotDrive[] getRobotdrive() {
+        return robotdrive;
+    }
+
+    public void setRobotdrive(RobotDrive[] robotdrive) {
+        this.robotdrive = robotdrive;
+    }
+
+
+
+
+}
     
     
 
-}
+
